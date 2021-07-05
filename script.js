2befa95dc29e3361551d1864f9fa296464ba4490
hkmichae <heathermichaels@icloud.com>
2021-07-05 15:36:29 -0400

    Js - profile cards function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const APIURL = 'https://api.github.com/users/'
 
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
@@ -10,9 +11,11 @@ async function getUser(username) {
     try {
         const { data } = await axios(APIURL + username)
 
-        console.log(data)
+        createUserCard(data)
     } catch(err) {
-        console.log(err)
+        if(err.response.status == 404) {
+            createErrorCard('No profile with this username')
+        }
     }
 }
 
@@ -32,14 +35,50 @@ async function getUser(username) {
     //         .catch(err => console.log(err))
     // }
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault()
 
-        const user = search.value
-        
-        if(user) {
-            getUser(user)
+function createUserCard(user) {
+    const cardHTML = `
+        <div class="card">
+                <div>
+                    <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+                </div>
 
-            search.value = ''
-        }
-    })
+                <div class="user-info">
+                    <h2>${user.name}</h2>
+                    <p>${user.bio}</p>
+
+                    <ul>
+                        <li>${user.followers} <strong>Followers</strong></li>
+                        <li>${user.following} <strong>Following</strong></li>
+                        <li>${user.public_repos} <strong>Repos</strong></li>
+                    </ul>
+
+                    <div id="repos"></div>
+                </div>
+            </div>
+    `
+
+    main.innerHTML = cardHTML
+}
+
+function createErrorCard(msg) {
+    const cardHTML = `
+        <div class="card">
+            <h1>${msg}</h1>
+        </div>
+    `
+
+    main.innerHTML = cardHTML
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = search.value
+    
+    if(user) {
+        getUser(user)
+
+        search.value = ''
+    }
+})
